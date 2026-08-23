ab8bc20314e55b4e55be359b7ad1f9cb36cadfd9
cassiellos <cassiellosagencia@gmail.com>
2026-08-23 14:53:36 -0300

    Integra pré-diagnóstico ao final da Home

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,11 @@
 import HomeContent from '@/components/home-content'
+import ContactIntakeForm from '@/components/contact/ContactIntakeForm'
 
 export default function Home() {
-  return <HomeContent lang="pt" />
+  return (
+    <>
+      <HomeContent lang="pt" />
+      <ContactIntakeForm lang="pt" />
+    </>
+  )
 }
